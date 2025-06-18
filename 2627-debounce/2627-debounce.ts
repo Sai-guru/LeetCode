b89f45e66012f999e6791cf8af938969c7a14955
SAI GURU_2006 <165147568+Sai-guru@users.noreply.github.com>
2025-06-18 11:27:09 +0530

    Time: 47 ms (73.04%), Space: 55.4 MB (61.93%) - LeetHub

---
+++ 2627-debounce/2627-debounce.ts
@@ -0,0 +1,11 @@
+type F = (...args: number[]) => void
+
+function debounce(fn: F, t: number): F {
+    let timeout;
+    return function(...args) {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            fn.apply(this, args);
+        }, t);
+    }
+};
