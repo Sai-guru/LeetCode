e45598a1748ee0fbf99eed7bb697756b717ce153
SAI GURU_2006 <165147568+Sai-guru@users.noreply.github.com>
2025-08-02 21:47:55 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (94.96%) - LeetSync

--- 136-single-number/single-number.js
+++ 136-single-number/single-number.js
@@ -4,18 +4,21 @@
  */
 var singleNumber = function(a) {
 
-    let hash = {};
+    // let hash = {};
+    // for(let i=0;i<a.length;i++) {
+    //     if(!hash[a[i]]) {
+    //          hash[a[i]] = 1;
+    //     } else {
+    //          hash[a[i]]++;
+    //     }
+    // }
+    // for(let i=0;i<a.length;i++) {
+    //    if( hash[a[i]] == 1) {
+    //     return a[i];
+    //    }
+    // }
+    let xorIdent = 0;
     for(let i=0;i<a.length;i++) {
-        if(!hash[a[i]]) {
-             hash[a[i]] = 1;
-        } else {
-             hash[a[i]]++;
-        }
-    }
-    for(let i=0;i<a.length;i++) {
-       if( hash[a[i]] == 1) {
-        return a[i];
-       }
-    }
-    
+       xorIdent = xorIdent^a[i];
+    }return xorIdent;
 };
