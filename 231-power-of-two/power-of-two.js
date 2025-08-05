249fd4dd00dc575df61a09ed8f4f3d120cb01be7
SAI GURU_2006 <165147568+Sai-guru@users.noreply.github.com>
2025-08-05 21:21:39 +0530

    Time: 1 ms (29.83%) | Memory: 57.5 MB (12.26%) - LeetSync

--- 231-power-of-two/power-of-two.js
+++ 231-power-of-two/power-of-two.js
@@ -6,10 +6,11 @@ var isPowerOfTwo = function(n) {
     if(n==1) return true;
     if(n<=0 || n%2!==0) return false;
 
-     let x = Math.log2(n);
-     return Number.isInteger(x);
-     
-    // if(2**x == n) return true;
-    if(Math.pow(2,x)==n) return true;
+    //  let x = Math.log2(n);
+    //  return Number.isInteger(x);
+
+    // // if(2**x == n) return true;
+    // if(Math.pow(2,x)==n) return true;
+    return isPowerOfTwo(n / 2);
     
 };
