8ae1e56023c98b8ed32730b788c6ade4fca4b2cd
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-08-11 21:10:55 +0530

    Time: 373 ms (7.85%) | Memory: 79.6 MB (64.98%) - LeetSync

---
+++ 948-sort-an-array/sort-an-array.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const sortArray = (arr)=> {
+    n =arr.length;
+    if(n<=1) return arr;
+    const mid = Math.floor(n/2);
+    let left = sortArray(arr.slice(0,mid));
+    let right = sortArray(arr.slice(mid));
+
+    return merge(left,right);
+
+}
+const merge=(arr1,arr2)=> {
+    let arr = [];
+    let m = arr1.length;
+    let n = arr2.length;
+    let p1 = m -1 ;
+    let p2 = n -1 ;
+
+    for(let i=(m+n)-1;i>=0;i--) {
+        if(p1>=0 && (p2<0 || arr1[p1]>arr2[p2])) {
+            arr[i] = arr1[p1];
+            p1--;
+        }else {
+            arr[i] = arr2[p2];
+            p2--;
+        }
+    }return arr
+}
