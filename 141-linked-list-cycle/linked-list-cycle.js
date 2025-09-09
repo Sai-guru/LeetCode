f5c47c314632e16ec9e28d039756e4508a87b086
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-09-09 20:23:56 +0530

    Time: 42 ms (95.23%) | Memory: 56.8 MB (99.67%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.js
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -13,15 +13,25 @@
 
 
 var hasCycle = function(head) {
-    let curr =head;
-    var set = new Set();
-    while(curr) {
-        if(set.has(curr)) return true;
-        set.add(curr);
-        curr = curr.next;
-    }
-    return false;
+    // let curr =head;
+    // var set = new Set();
+    // while(curr) {
+    //     if(set.has(curr)) return true;
+    //     set.add(curr);
+    //     curr = curr.next;
+    // }
+    // return false;
 
+       if(!head) return false;
+    let slow = head;
+    let fast = head.next;
 
-    
+    while(slow!=fast) {
+          if(fast == null || fast.next ==null) return false;
+        slow =  slow.next;
+        fast =  fast.next.next;
+    }return true;
 };
+
+
+
