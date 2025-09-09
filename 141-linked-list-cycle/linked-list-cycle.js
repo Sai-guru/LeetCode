82b1853be771973bcd653e59466da25da2d7f4d4
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-09-09 19:12:40 +0530

    Time: 61 ms (14.63%) | Memory: 59 MB (68.19%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+
+var hasCycle = function(head) {
+    let curr =head;
+    var set = new Set();
+    while(curr) {
+        if(set.has(curr)) return true;
+        set.add(curr);
+        curr = curr.next;
+    }
+    return false;
+
+
+    
+};
