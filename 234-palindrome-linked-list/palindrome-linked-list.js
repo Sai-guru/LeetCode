b68bcab49637576d20faf4d722387e375f4dd23d
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-09-10 22:28:29 +0530

    Time: 5 ms (63.55%) | Memory: 77.4 MB (60.50%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.js
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -13,19 +13,72 @@
 
 var isPalindrome = function(head) {
      
-     let arr = [];
-    let curr =  head;
-    while(curr) {
-        arr.push(curr.val);
-        curr =  curr.next;
-    }
-    // let newArr = [...arr];
-    let newArr = [...arr].reverse(); 
-    // let n =  newArr.length;
-    // for(let i=0;i<Math.floor(n/2);i++) {
-    //     let temp = newArr[i];
-    //     newArr[i] = newArr[n-(i+1)];
-    //     newArr[n-(i+1)] = temp;
+    //  let arr = [];
+    // let curr =  head;
+    // while(curr) {
+    //     arr.push(curr.val);
+    //     curr =  curr.next;
     // }
-      return arr.join() === newArr.join();
+    // // let newArr = [...arr];
+    // let newArr = [...arr].reverse(); 
+    // // let n =  newArr.length;
+    // // for(let i=0;i<Math.floor(n/2);i++) {
+    // //     let temp = newArr[i];
+    // //     newArr[i] = newArr[n-(i+1)];
+    // //     newArr[n-(i+1)] = temp;
+    // // }
+    //   return arr.join() === newArr.join();
+
+    //1st : find the middle
+    let slow = fast = head;
+    while(fast && fast.next) {
+        slow =  slow.next;
+        fast =  fast.next.next;
+    } //now slow the middle node(element)
+
+    //2nd : Reverse the 2nd half of the linked list
+
+    let prev = null;
+    let curr = slow;
+    while(curr) {
+        let temp =  curr.next;
+        curr.next =  prev;
+        prev = curr;
+        curr = temp;
+    }   //now prev is the end of the whole linked list OR 
+    // we can say as head of the reversed 2nd half ll
+
+
+    //3rd : compare the both from head and end and reach near to check this ...
+
+    let firstll =  head;
+    let secondll =  prev;
+
+    while(secondll) {
+        if(firstll.val !=  secondll.val) return false;
+
+        firstll = firstll.next;
+        secondll = secondll.next;
+
+    } return true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 };
