767d129245ad922a1d9b1d490b0c30c47d1ae903
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-09-10 20:14:35 +0530

    Time: 68 ms (12.24%) | Memory: 85.8 MB (12.46%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+
+var isPalindrome = function(head) {
+     
+     let arr = [];
+    let curr =  head;
+    while(curr) {
+        arr.push(curr.val);
+        curr =  curr.next;
+    }
+    let newArr = [...arr];
+    let n =  newArr.length;
+    for(let i=0;i<Math.floor(n/2);i++) {
+        let temp = newArr[i];
+        newArr[i] = newArr[n-(i+1)];
+        newArr[n-(i+1)] = temp;
+    }
+    // if(arr === newArr) return true ;
+    // else false ;
+      return arr.join() === newArr.join();
+};
