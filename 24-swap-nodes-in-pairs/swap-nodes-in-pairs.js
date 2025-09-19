8fcf47f4b088da60f3615cbbb8fd299aecfcc6dc
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-09-19 17:27:38 +0530

    Time: 0 ms (100.00%) | Memory: 54.2 MB (21.55%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -9,26 +9,62 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
+// var swapPairs = function(head) {
+
+//     if(!head) return head;
+
+//     let dummyNode = new ListNode();
+//     dummyNode.next = head;
+//     let curr =  dummyNode;
+
+//     while(curr.next && curr.next.next) {
+//         let first  = curr.next;
+//         let second = curr.next.next;
+//         let temp = second.next;
+
+//         curr.next = second;
+//         second.next = first;
+//         first.next = temp;
+
+//         curr = first;
+
+//     }
+//     return dummyNode.next;
+// };
+
 var swapPairs = function(head) {
 
-    if(!head) return head;
+    if(!head || !head.next) return head;
 
-    let dummyNode = new ListNode();
-    dummyNode.next = head;
-    let curr =  dummyNode;
 
-    while(curr.next && curr.next.next) {
-        let first  = curr.next;
-        let second = curr.next.next;
-        let temp = second.next;
+    let l =  head;
+    let r = head.next;
 
-        curr.next = second;
-        second.next = first;
-        first.next = temp;
+    l.next = swapPairs(r.next);
+    r.next = l;
 
-        curr = first;
-        
-    }
-    return dummyNode.next;
-    
-};
+    return r;
+     
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
