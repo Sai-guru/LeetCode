27edd5553781ceb2a53d28d70291cab5d31f4b30
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-09-20 23:10:35 +0530

    Time: 0 ms (100.00%) | Memory: 55.1 MB (8.09%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -4,9 +4,29 @@
  */
 var lengthOfLastWord = function(s) {
 
-    s = s.trim().split(" ");
-    let res = s[s.length-1].length;
+    // s = s.trim().split(" ");
+    // let res = s[s.length-1].length;
 
-    return res;
+    // return res;
+     
+    let n = s.length-1;
+    //trim the empty spaces at the end...
+    //traversing from end (bcos easy and fast)...
+
+    while(n>=0){
+        if(s[n]==" ") n--;
+        else break;
+    }
+   //count the length - means that after the spaces crossing we enter into the word, 
+    //for each word's index increment the count so that we can get the length of it..
+       let count = 0;
+    while(n>=0) {
+        if(s[n] != " ") {
+            count++;
+            n--;
+        }else break;
+    }
+       return count;
+
     
 };
