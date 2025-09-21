2792860a2ae9972045d56a2657c921ea84db55ec
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-09-21 16:01:32 +0530

    Time: 1 ms (86.27%) | Memory: 57.6 MB (61.99%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.js
+++ 3194-find-words-containing-character/find-words-containing-character.js
@@ -7,9 +7,16 @@ var findWordsContaining = function(words, x) {
 
     let res = [];
     for(let i=0;i<words.length;i++){
-        if(words[i].includes(x)){
-            res.push(i)
+        // if(words[i].includes(x))  res.push(i);
+         let singleWord = words[i];
+        for(let j=0;j<singleWord.length;j++) {
+            if(singleWord[j] === x) {
+                res.push(i);
+                break
+            }
         }
+           
+        
     }return res;
     
 };
