0a00ac2ad5ea6a0c4f85f4165461afdd958126d9
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-09-22 12:17:44 +0530

    Time: 0 ms (100.00%) | Memory: 56 MB (5.19%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.js
+++ 782-jewels-and-stones/jewels-and-stones.js
@@ -5,13 +5,24 @@
  */
 var numJewelsInStones = function(jewels, stones) {
     
-    jewels = jewels.split('');
+    // jewels = jewels.split('');
+    // stones = stones.split('');
+    // let count = 0;
+
+    // for(let i=0;i<stones.length;i++) {
+    //     // if(jewels.includes(stones[i])) count++;
+    //     for(let j=0;j<jewels.length;j++) {
+    //         if(stones[i]===jewels[j]) count++;
+    //     }
+    // }return count;
+
     stones = stones.split('');
+    let jewelsSet = new Set(jewels);
     let count = 0;
 
-    for(let i=0;i<stones.length;i++) {
-        for(let j=0;j<jewels.length;j++) {
-            if(stones[i]===jewels[j]) count++;
-        }
+    for(let i=0;i<stones.length;i++){
+        if(jewelsSet.has(stones[i])) count++;
+
     }return count;
+
 };
