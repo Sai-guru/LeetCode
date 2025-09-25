fa2c32382db1efec900e00a8b78d1e31c984b479
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-09-25 19:20:44 +0530

    Time: 5 ms (62.35%) | Memory: 56.4 MB (63.84%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.js
+++ 541-reverse-string-ii/reverse-string-ii.js
@@ -6,10 +6,12 @@
 var reverseStr = function(s, k) {
     
   s = s.split('');
+  //1st loop for jump to 2k elements
   for(let x=0;x<s.length;x+=(2*k)) {
     let n = k;
     let mid =  Math.floor(n/2);
 
+//2nd loop for swap processing with starting k elements accessed by x...
     for(let i=0;i<mid;i++) {   ///just add x , we know this logic by default...
         let temp = s[x+i];
         s[x+i] = s[x+n-(i+1)];
