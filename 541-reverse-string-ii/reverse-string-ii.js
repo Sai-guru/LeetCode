c80643318553e79d1d8d8ccdde4e51990c396151
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-09-25 18:57:20 +0530

    Time: 6 ms (46.71%) | Memory: 55.9 MB (81.88%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function(s, k) {
+    
+  s = s.split('');
+  for(let x=0;x<s.length;x+=(2*k)) {
+    let n = k;
+    let mid =  Math.floor(n/2);
+
+    for(let i=0;i<mid;i++) {   ///just add x , we know this logic by default...
+        let temp = s[x+i];
+        s[x+i] = s[x+n-(i+1)];
+        s[x+n-(i+1)]= temp;
+    }
+  }
+  return s.join('');
+    
+}; 
