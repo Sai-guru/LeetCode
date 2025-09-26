26704d45c7ae4cbfe15a705a10ac5c163b29cf5e
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-09-26 12:52:30 +0530

    Time: 15 ms (8.17%) | Memory: 64 MB (7.45%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -6,46 +6,17 @@ var isPalindrome = function(s) {
      
     s = s.toLowerCase();
     let filteredStr = "";
+    let revStr = "";
 
     for(let i=0;i<s.length;i++){
         if(s[i].match(/[a-z0-9]/i)) {
             filteredStr += s[i];
+            // rev = s[i]+ rev;  single line instead of 2nd for loop
         }
     }
-
-    console.log(filteredStr)
     // let revStr =  [...filteredStr].reverse().join('');
-    let revStr = "";
     for(let i= filteredStr.length-1;i>=0;i--){
         revStr += filteredStr[i];
     }
-    console.log(revStr);
     return filteredStr === revStr;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // s = s.toLowerCase().split('');
-    // let main = [];
-
-    // for(let i=0;i<s.length;i++){
-
-    //     if(s[i].match(/[a-z0-9]/i)) main.push(s[i]);
-        
-    // }
-    // let revStr =  [...main].reverse().join('');
-    
-    // return main.join('') === revStr;
 }; 
