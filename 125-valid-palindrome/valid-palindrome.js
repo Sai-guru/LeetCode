93080911732694ee1cea1298753fcfa55d28d668
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-09-26 16:40:58 +0530

    Time: 9 ms (23.37%) | Memory: 62.1 MB (15.48%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,24 +3,21 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
+    s = s.toLowerCase();
+    let i = 0;
+    let j = s.length-1;
 
-   s = s.toLowerCase();
-   let i=0;
-   let j = s.length-1;
-   while(i<j) {
-    if(!s[i].match(/[a-z0-9]/i)) i++;
-    else if(!s[j].match(/[a-z0-9]/i)) j--;
+    while(i<j) {
 
-    else if(s[i]===s[j]) {
-        // let temp = s[i];
-        // s[i] = s[j];
-        // s[j] = temp;
-        j--;
-        i++;
-    }else {
-        return false;
+        if(!s[i].match(/[a-z0-9]/i)) i++;
+        else if(!s[j].match(/[a-z0-9]/i)) j--;
+        else if(s[i]==s[j]) {
+            i++;j--;
+        }
+        else return false;
     }
-   } return true;
+    return true;
+
 
 
 }; 
