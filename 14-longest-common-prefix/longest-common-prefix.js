3fb3fe52a06f9594b171066f9ccbed258bd6277f
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-09-28 11:14:42 +0530

    Time: 9 ms (5.97%) | Memory: 55.7 MB (29.95%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -4,32 +4,32 @@
  */
 var longestCommonPrefix = function(str) {
 
-    // if(str.length<=0) return "";
-    // str.sort();
-    // console.log(str)
-
-    // let res = "";
-    // let first = str[0];
-    // let last = str[str.length-1];
-    // let i=0;
-    // while(i<first.length && i<last.length) {
-    //     if(first[i]==last[i]) {
-    //         res+=first[i];
-    //         i++
-    //     }else {
-    //         break;
-    //     }
-    // }return res;
-
-    let x =0 ;
     if(str.length<=0) return "";
-    while(x<str[0].length){
-        let ch = str[0][x];
-        for(let i=1;i<str.length;i++) {
-            if(ch != str[i][x] || x == str[i].length) {
-                 return str[0].slice(0,x);
-            }
+    str.sort();
+    console.log(str)
+
+    let res = "";
+    let first = str[0];
+    let last = str[str.length-1];
+    let i=0;
+    while(i<first.length && i<last.length) {
+        if(first[i]==last[i]) {
+            res+=first[i];
+            i++
+        }else {
+            break;
         }
-        x++;
-    }return str[0];
+    }return res;
+
+    // let x =0 ;
+    // if(str.length<=0) return "";
+    // while(x<str[0].length){
+    //     let ch = str[0][x];
+    //     for(let i=1;i<str.length;i++) {
+    //         if(ch != str[i][x] || x == str[i].length) {
+    //              return str[0].slice(0,x);
+    //         }
+    //     }
+    //     x++;
+    // }return str[0];
 };
