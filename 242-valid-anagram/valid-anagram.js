a4e74f67d7a1c8946e2b152bdb26f337ff1fa4a2
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-09-29 13:41:19 +0530

    Time: 39 ms (5.53%) | Memory: 59.7 MB (25.60%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -6,19 +6,19 @@
 var isAnagram = function(s, t) {
 
     if(s.length!==t.length) return false;
-    let res =  true;
 
     const sArr = [...s];
     sArr.sort();
+    console.log(sArr);
     const tArr = [...t];
     tArr.sort();
+     console.log(tArr);
 
     for(let i=0;i<sArr.length;i++) {
-        if(sArr[i]!==tArr[i]) {
-            res = false;
-            return res;
-        }
-    } return res;
+        if(sArr[i]!==tArr[i]) return false;
+        
+    } return true;
+
 
 };
 
