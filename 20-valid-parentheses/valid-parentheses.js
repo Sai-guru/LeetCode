297f28896526c5989cd640e532ec2f794cad489d
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-10-06 20:46:13 +0530

    Time: 4 ms (48.17%) | Memory: 57.8 MB (21.63%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -3,17 +3,29 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let stack = [];
-    const map = { '(':')','[':']','{':'}' };
+    // let stack = [];
+    // const map = { '(':')','[':']','{':'}' };
 
-    for(let i=0;i<s.length;i++){
+    // for(let i=0;i<s.length;i++){
+    //     let currSymbol = s[i];
+    //     if(currSymbol =='(' || currSymbol == '[' || currSymbol == '{') {
+    //         stack.push(currSymbol);
+    //     }else {
+    //         if(stack.length == 0 || currSymbol !== map[stack.pop()]) return false;
+    //     }
+    // }
+    // return stack.length == 0;
+    
+    let stack = [];
+    for(let i=0;i<s.length;i++) {
         let currSymbol = s[i];
-        if(currSymbol =='(' || currSymbol == '[' || currSymbol == '{') {
+         if(currSymbol =='(' || currSymbol == '[' || currSymbol == '{') {
             stack.push(currSymbol);
         }else {
-            if(stack.length == 0 || currSymbol !== map[stack.pop()]) return false;
+            let top = stack.pop();
+            if(!top || (top == '(' && currSymbol!== ')'|| top == '[' && currSymbol!== ']' || top == '{' && currSymbol!== '}'))return false;
         }
-    }
-    return stack.length == 0;
+
+    }return stack.length == 0;
     
 };
