c29e4fa5f4058d61db1110c995d5de773a2ec8e0
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2025-10-13 19:33:23 +0530

    Time: 27 ms (40.32%) | Memory: 83.9 MB (7.75%) - LeetSync

---
+++ 739-daily-temperatures/daily-temperatures.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temp) {
+    let stack = [];
+    let res = [];
+    let n = temp.length;
+    stack.push([temp[n-1],n-1]);
+    res.push(0);
+
+    for(let i=n-2;i>=0;i--) {
+        
+        while(stack.length && temp[i]>=stack[stack.length-1][0]) {
+            stack.pop();
+           
+        }
+        if(stack.length==0) res.push(0);
+        else res.push(stack[stack.length-1][1]-i);
+        stack.push([temp[i],i]);
+        
+    }
+    return res.reverse();
+
+
+
+};
