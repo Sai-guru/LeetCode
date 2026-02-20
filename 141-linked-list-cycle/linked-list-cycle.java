5f8aa373249f108f42aa6055741115825a702f01
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-02-20 21:42:30 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (86.91%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -12,16 +12,36 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
 
-        ListNode curr = head;
-        Set<ListNode> mySet = new HashSet<>();
+        // ListNode curr = head;
+        // Set<ListNode> mySet = new HashSet<>();
 
-        while(curr!=null) {
-            if(mySet.contains(curr)) return true;
+        // while(curr!=null) {
+        //     if(mySet.contains(curr)) return true;
 
-            mySet.add(curr);
-            curr = curr.next;
+        //     mySet.add(curr);
+        //     curr = curr.next;
+        // }
+        // return false;
+
+        //Approach 2 - Floyd's cycle
+        
+        if(head==null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while(slow!=fast) {
+            if(fast==null || fast.next==null) return false;
+            
+            fast = fast.next.next;
+            slow = slow.next;
+
         }
-        return false;
+
+        return true;
+
+       
+
 
 
         
