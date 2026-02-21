5bb9da9ba61f34f63c5d4918b1aec82863c4eee7
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-02-21 13:00:28 +0530

    Time: 108 ms (5.03%) | Memory: 88.1 MB (5.14%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.js
+++ 234-palindrome-linked-list/palindrome-linked-list.js
@@ -13,54 +13,80 @@
 
 var isPalindrome = function(head) {
      
-    //  let arr = [];
-    // let curr =  head;
-    // while(curr) {
-    //     arr.push(curr.val);
-    //     curr =  curr.next;
-    // }
-    // // let newArr = [...arr];
-    // let newArr = [...arr].reverse(); 
-    // // let n =  newArr.length;
-    // // for(let i=0;i<Math.floor(n/2);i++) {
-    // //     let temp = newArr[i];
-    // //     newArr[i] = newArr[n-(i+1)];
-    // //     newArr[n-(i+1)] = temp;
-    // // }
-    //   return arr.join() === newArr.join();
+    let arr = [];
+    let curr = head;
+
+    while(curr) {
+
+        arr.push(curr.val);
+        curr = curr.next;
+    }
+
+    let newArr = [...arr];
+    let n = newArr.length;
+
+    for(let i=0;i<Math.floor(n/2);i++) {
+        let temp = newArr[i];
+        newArr[i] = newArr[n-(i+1)];
+        newArr[n-(i+1)] = temp;
+    }
+
+    return arr.join()===newArr.join();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //1st : find the middle
-    let slow = fast = head;
-    while(fast && fast.next) {
-        slow =  slow.next;
-        fast =  fast.next.next;
-    } //now slow the middle node(element)
+    // let slow = fast = head;
+    // while(fast && fast.next) {
+    //     slow =  slow.next;
+    //     fast =  fast.next.next;
+    // } //now slow the middle node(element)
 
-    //2nd : Reverse the 2nd half of the linked list
+    // //2nd : Reverse the 2nd half of the linked list
 
-    let prev = null;
-    let curr = slow;
-    while(curr) {
-        let temp =  curr.next;
-        curr.next =  prev;
-        prev = curr;
-        curr = temp;
-    }   //now prev is the end of the whole linked list OR 
-    // we can say as head of the reversed 2nd half ll
+    // let prev = null;
+    // let curr = slow;
+    // while(curr) {
+    //     let temp =  curr.next;
+    //     curr.next =  prev;
+    //     prev = curr;
+    //     curr = temp;
+    // }   //now prev is the end of the whole linked list OR 
+    // // we can say as head of the reversed 2nd half ll
 
 
-    //3rd : compare the both from head and end and reach near to check this ...
+    // //3rd : compare the both from head and end and reach near to check this ...
 
-    let firstll =  head;
-    let secondll =  prev;
+    // let firstll =  head;
+    // let secondll =  prev;
 
-    while(secondll) {
-        if(firstll.val !=  secondll.val) return false;
+    // while(secondll) {
+    //     if(firstll.val !=  secondll.val) return false;
 
-        firstll = firstll.next;
-        secondll = secondll.next;
+    //     firstll = firstll.next;
+    //     secondll = secondll.next;
 
-    } return true;
+    // } return true;
 
 
 
