b21dc02bcfe4ac972b665647b11a4ec72dd0e016
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-02-24 20:04:40 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (83.71%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.js
@@ -10,23 +10,52 @@
  * @param {number} n
  * @return {ListNode}
  */
+//  ----my weird idea---
+
+    // let size = 0;
+    // let curr = head;
+
+    // while(curr) {
+    //     size++;
+    //     curr = curr.next;
+    // }
+
+    // if(n==size) return head.next;
+    // curr = head;
+    // for(let i=0;i<size-(n+1);i++) {
+    //     curr = curr.next;
+    // }
+    // curr.next = curr.next.next;
+    // return head;
+
+
+
 var removeNthFromEnd = function(head, n) {
 
-    let size = 0;
+    // same as above , but just for making senti;
+
+    let senti = new ListNode();
+    senti.next = head;
+    let prev = senti;
     let curr = head;
+    let size = 0;
 
     while(curr) {
         size++;
         curr = curr.next;
     }
-
     if(n==size) return head.next;
-    curr = head;
-    for(let i=0;i<size-(n+1);i++) {
-        curr = curr.next;
+    let prevOfTarget = size-n;
+
+    for(let i=0;i<prevOfTarget;i++) {
+        prev = prev.next;
     }
-    curr.next = curr.next.next;
+
+    prev.next = prev.next.next;
     return head;
+
+
+
 
     
 
