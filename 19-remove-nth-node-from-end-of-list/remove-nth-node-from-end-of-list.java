d37470215ca490fc755ed0eca362a1235e83f483
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-02-24 20:05:50 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (77.37%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -8,24 +8,45 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+// ------My weird idea-----
+//   int size = 0;
+//         ListNode curr = head;
+
+//         while(curr!=null) {
+//             size++;
+//             curr = curr.next;
+//         }
+//         if(n == size) return head.next;
+//         curr = head;
+
+//         for(int i=0;i<size-(n+1);i++) {
+//             curr = curr.next;
+//         }
+//         curr.next = curr.next.next;
+//         return head;
+
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+     
+    ListNode senti = new ListNode();
+    senti.next = head;
+    ListNode prev = senti;
+    ListNode curr = head;
+    int size = 0;
 
-        int size = 0;
-        ListNode curr = head;
-
-        while(curr!=null) {
-            size++;
-            curr = curr.next;
-        }
-        if(n == size) return head.next;
-        curr = head;
-
-        for(int i=0;i<size-(n+1);i++) {
-            curr = curr.next;
-        }
-        curr.next = curr.next.next;
-        return head;
-        
+    while (curr != null) {
+        size++;
+        curr = curr.next;
     }
+    if (n == size) return head.next;
+    int prevOfTarget = size - n;
+
+    for (int i = 0; i < prevOfTarget; i++) {
+        prev = prev.next;
+    }
+
+    prev.next = prev.next.next;
+    return head;
+}   
 }
