63895a1d6aad961289c1add839cf0326079faad2
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-02-24 20:32:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (38.66%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.java
@@ -26,27 +26,51 @@
 //         curr.next = curr.next.next;
 //         return head;
 
+// ---almost same as above , just used senti and place standing ..
+// ListNode senti = new ListNode();
+//     senti.next = head;
+//     ListNode prev = senti;
+//     ListNode curr = head;
+//     int size = 0;
+
+//     while (curr != null) {
+//         size++;
+//         curr = curr.next;
+//     }
+//     if (n == size) return head.next;
+//     int prevOfTarget = size - n;
+
+//     for (int i = 0; i < prevOfTarget; i++) {
+//         prev = prev.next;
+//     }
+
+//     prev.next = prev.next.next;
+//     return head;
+
+
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        ListNode senti = new ListNode();
+        senti.next = head;
+        ListNode first = senti;
+        ListNode second = senti;
+
+        for(int i=0;i<n;i++) {
+            first = first.next;
+        }
+
+        while(first!=null && first.next!=null) {
+            first = first.next;
+            second = second.next;
+        }
+
+        second.next = second.next.next;
+        return senti.next;
+
+
+
      
-    ListNode senti = new ListNode();
-    senti.next = head;
-    ListNode prev = senti;
-    ListNode curr = head;
-    int size = 0;
-
-    while (curr != null) {
-        size++;
-        curr = curr.next;
-    }
-    if (n == size) return head.next;
-    int prevOfTarget = size - n;
-
-    for (int i = 0; i < prevOfTarget; i++) {
-        prev = prev.next;
-    }
-
-    prev.next = prev.next.next;
-    return head;
+    
 }   
 }
