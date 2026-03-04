cb149db89a9cb04c53eb185b823e981e709fa40d
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-03-04 20:31:17 +0530

    Time: 0 ms (100.00%) | Memory: 57.3 MB (55.07%) - LeetSync

--- 61-rotate-list/rotate-list.js
+++ 61-rotate-list/rotate-list.js
@@ -11,35 +11,49 @@
  * @return {ListNode}
  */
 var rotateRight = function(head, k) {
-       
-       
-        if(!head || !head.next) return head;
 
-// first get the length(size);
+    if(!head || !head.next) return head;
 
-    let length = 0;
-    let curr =  head;
+    //first know the length of the LL
+
+    let len = 0;
+    let curr = head;
+
     while(curr) {
-        length++;
-        curr =  curr.next;
-        }
-        //remove unwanted rots..
-    k = k%length;
+        len++;
+        curr = curr.next;
+    }
 
-//maai -- our known logic code ...
-    let fast =  slow = head;
+    //avoid unwanted rotates
+    k = k%len;
+
+    //our reaching logic , 
+    //1st reach the place of last node using fast pointer with k 
+    let fast = slow = head;
 
     for(let i=0;i<k;i++) {
         fast = fast.next;
     }
 
+    //now the above last node is reached by that fast pointer
+    //so , run the  fast and slow pointers , where 
+    //the slow pointers will take the last node position from the fast pointer
     while(fast.next) {
         fast = fast.next;
         slow = slow.next;
     }
+
+    //now form the cycle by fast last ptr to given head;
+    //slow next pointer to newHead (bcos then only can keep the starting node)
+    //then make the slow pointer next to null - bcos end of the list
+    
     fast.next = head;
     let newHead = slow.next;
     slow.next = null;
 
-    return newHead; 
-};
+    //simply return the newHead;
+
+    return newHead;
+
+       
+}
