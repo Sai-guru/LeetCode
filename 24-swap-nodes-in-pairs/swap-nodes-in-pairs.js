da7027357d345e997e9ff0601de1cc2073ec357e
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-03-05 21:06:31 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (39.11%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.js
@@ -9,33 +9,9 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-// var swapPairs = function(head) {
-
-//     if(!head) return head;
-
-//     let dummyNode = new ListNode();
-//     dummyNode.next = head;
-//     let curr =  dummyNode;
-
-//     while(curr.next && curr.next.next) {
-//         let first  = curr.next;
-//         let second = curr.next.next;
-//         let temp = second.next;
-
-//         curr.next = second;
-//         second.next = first;
-//         first.next = temp;
-
-//         curr = first;
-
-//     }
-//     return dummyNode.next;
-// };
-
 var swapPairs = function(head) {
 
     if(!head || !head.next) return head;
-
 
     let l =  head;
     let r = head.next;
@@ -44,27 +20,5 @@ var swapPairs = function(head) {
     r.next = l;
 
     return r;
-     
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
+};
