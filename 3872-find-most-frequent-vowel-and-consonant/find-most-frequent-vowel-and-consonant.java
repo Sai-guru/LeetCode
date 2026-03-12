e152dd11332412116b6b5db15ef40ea79efb6004
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-03-12 09:43:24 +0530

    Time: 4 ms (23.80%) | Memory: 44.1 MB (29.50%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.java
@@ -3,36 +3,24 @@ class Solution {
 
         int maxV = 0,maxC = 0;
         Map<Character,Integer> map = new HashMap<>();
-        Set<Character> vowelSet = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
-
 
         for(int i=0;i<s.length();i++) {
             char x = s.charAt(i);
             
             if(map.containsKey(x)) map.put(x,map.get(x)+1);
             else map.put(x,1);
+            
+        }
 
+        // Set<Character> vowelSet = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        String vowels = "aeiou";
 
-            if(vowelSet.contains(x)) maxV = Math.max(maxV,map.get(x));
+        for(char x : map.keySet()) {
+            
+            if(vowels.indexOf(x)!=-1) maxV = Math.max(maxV,map.get(x));
             else maxC = Math.max(maxC,map.get(x));
         }
 
-       
-       
-            
-            
-       
-
-        return maxV+maxC;
-
-
-
-
-
-        
-
-
-
-        
+        return maxV+maxC;     
     }
 }
