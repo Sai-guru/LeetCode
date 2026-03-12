77df3b6649fd9ba257303e3b39fbe82cde2efefd
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-03-12 08:49:11 +0530

    Time: 7 ms (66.19%) | Memory: 56.8 MB (73.72%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.js
@@ -3,20 +3,24 @@
  * @return {number}
  */
 var maxFreqSum = function(s) {
-    
-    let map = {};
 
-    for(let i=0;i<s.length;i++){
+    let map = {};
+    let maxV = maxC = 0;
+    const vowels = ['a','e','i','o','u'];
+
+    for(let i=0;i<s.length;i++) {
+
         if(!map[s[i]]) map[s[i]]=1;
         else map[s[i]]++;
     }
 
-    let maxC = maxV = 0;
-    let vowels = ['a','e','i','o','u'];
-    let mapKeys = Object.keys(map);
-
+    const mapKeys = Object.keys(map);
+    
     for(let i=0;i<mapKeys.length;i++) {
+
         if(vowels.includes(mapKeys[i])) maxV = Math.max(maxV,map[mapKeys[i]]);
         else maxC = Math.max(maxC,map[mapKeys[i]]);
-    }return maxV+maxC;
+    }
+
+    return maxV+maxC;  
 };
