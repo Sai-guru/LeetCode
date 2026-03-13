40e3c1cba3aabf25326e190bdc7f6717834c82a1
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-03-13 21:17:09 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (55.57%) - LeetSync

--- 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.js
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.js
@@ -3,17 +3,19 @@
  * @return {number}
  */
 var balancedStringSplit = function(s) {
-    let r = l = 0;
-    let count = 0;
-    
-    for(let i=0;i<s.length;i++){
+
+    let l = r = 0;
+    let c = 0;
+
+    for(let i=0;i<s.length;i++) {
         if(s[i]=='R') r++;
         else l++;
 
-        if(l==r){
-            count++;
+        if(l==r) {
+            c++;
             l = r = 0;
-        }   
-    }return count;
-
+        }
+    }
+    return c;
+    
 };
