3bdcee19ebce92aa08429a2f579ac770c90badb6
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-03-14 13:51:10 +0530

    Time: 5 ms (55.20%) | Memory: 56 MB (69.26%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.js
+++ 541-reverse-string-ii/reverse-string-ii.js
@@ -4,20 +4,25 @@
  * @return {string}
  */
 var reverseStr = function(s, k) {
-    
-  s = s.split('');
-  //1st loop for jump to 2k elements
-  for(let x=0;x<s.length;x+=(2*k)) {
-    let n = k;
-    let mid =  Math.floor(n/2);
 
-//2nd loop for swap processing with starting k elements accessed by x...
-    for(let i=0;i<mid;i++) {   ///just add x , we know this logic by default...
-        let temp = s[x+i];
-        s[x+i] = s[x+n-(i+1)];
-        s[x+n-(i+1)]= temp;
+    s = s.split('')
+
+    for(let i=0;i<s.length;i+=2*k) {
+       //1st loop for jump to 2k elements
+        let n = k;
+        let mid = Math.floor(n/2);
+
+    // 2nd loop for swap processing with starting k elements accessed by x...
+        for(let j=0;j<mid;j++){   //just add i,we know this logic by default...
+
+            let temp = s[i+j];
+            s[i+j] = s[i+n-(j+1)];
+            s[i+n-(j+1)] = temp;
+
+        }
     }
-  }
-  return s.join('');
+    return s.join('');
+
+     
     
-}; 
+};
