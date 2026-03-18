e2c44e486e358b9458fdccafbf6df24f5f1371f9
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-03-18 13:23:03 +0530

    Time: 9 ms (85.35%) | Memory: 54.7 MB (94.45%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -6,30 +6,26 @@
 var isAnagram = function(s, t) {
 
     if(s.length!==t.length) return false;
-    // const sArr = [...s].sort();
-    // const tArr = [...t].sort();
-    // for(let i=0;i<sArr.length;i++) {
-    //     if(sArr[i]!==tArr[i]) return false;
-    // } return true;
 
     let sMap = {};
-    let tMap = {};
+    let tMap = {}
+
     for(let i=0;i<s.length;i++) {
-        if(!sMap[s[i]]) sMap[s[i]] =1;
+
+        if(!sMap[s[i]]) sMap[s[i]]=1;
         else sMap[s[i]]++;
 
-        if(!tMap[t[i]]) tMap[t[i]] =1;
-        else tMap[t[i]]++;
+        if(!tMap[t[i]]) tMap[t[i]]=1;
+        else tMap[t[i]]++
     }
 
-    let keys = Object.keys(sMap);
-    for(let i=0;i<keys.length;i++) {
+    let keyArr = Object.keys(sMap);
+    for(let i=0;i<keyArr.length;i++) {
 
-        if(sMap[keys[i]]!==tMap[keys[i]]) return false;
+        if(sMap[keyArr[i]]!==tMap[keyArr[i]]) return false;
     }
     return true;
 
-};
-
 
     
+};
