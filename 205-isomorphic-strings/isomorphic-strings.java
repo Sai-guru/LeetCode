b2b290fcf9568d7e8204604db0be88fa55ef2bcb
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-03-19 15:13:52 +0530

    Time: 19 ms (18.14%) | Memory: 44.2 MB (13.72%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+        if(s.length()!=t.length()) return false;
+
+        Map<Character,Character> s2T = new HashMap<>();
+        Map<Character,Character> t2S = new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+
+            if (s2T.containsKey(sChar) != t2S.containsKey(tChar)) return false;
+
+            if (!s2T.containsKey(sChar) && !t2S.containsKey(tChar)) {
+                s2T.put(sChar, tChar);
+                t2S.put(tChar, sChar);
+            } 
+
+            else if(s2T.get(sChar)!=tChar || t2S.get(tChar)!=sChar) return false;
+        }
+
+        return true;
+
+    }
+}
