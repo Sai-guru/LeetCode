8b85026af6b215d95458f41a5ff7019254e472d9
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-03-20 12:53:17 +0530

    Time: 19 ms (96.17%) | Memory: 69.3 MB (37.03%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.ts
@@ -0,0 +1,22 @@
+function groupAnagrams(strs: string[]): string[][] {
+
+
+       const map = new Map();
+
+    for(let i=0;i<strs.length;i++) {
+        let curr = strs[i].split('').sort().join('');
+
+        if(!map.has(curr)) map.set(curr,[]);
+
+        map.get(curr).push(strs[i]);
+    }
+
+    // console.log(map);
+
+    // console.log([...map.values()]);
+
+    return [...map.values()];
+
+    // or return Array.from(map.values());
+    
+};
