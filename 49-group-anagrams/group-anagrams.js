76d62ddb74fc3a8a96f8b6538f4d1bafcba0cfe8
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-03-20 12:50:11 +0530

    Time: 31 ms (63.78%) | Memory: 67.7 MB (57.59%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -2,25 +2,29 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-
-
 var groupAnagrams = function(strs) {
-    let map = {};
-    for(let i=0;i<strs.length;i++){
-        // create a key stored in an arr
-        let freqArr = Array(26).fill(0);  //[0,0,0,.....26th 0];
-        let currS = strs[i];
-        for(j=0;j<currS.length;j++) {
-            let index  = currS[j].charCodeAt() - 'a'.charCodeAt();
-            freqArr[index]++;
-        }
-        let key = '';
-        for(let i=0;i<26;i++) {
-            key+= String.fromCharCode(i)+freqArr[i];
-        }
-        if(!map[key]) map[key] = [currS];
-        else map[key].push(currS);
 
+    const map = new Map();
+
+    for(let i=0;i<strs.length;i++) {
+        let curr = strs[i].split('').sort().join('');
+
+        if(!map.has(curr)) map.set(curr,[]);
+
+        map.get(curr).push(strs[i]);
     }
-    return [...Object.values(map)];
+
+    // console.log(map);
+
+    // console.log([...map.values()]);
+
+    return [...map.values()];
+
+    // or return Array.from(map.values());
+
+    
+
+    
+
+    
 };
