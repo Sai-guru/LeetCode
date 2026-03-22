4decee3a4fac4283f63d3b52dd4ae2635cee0df0
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-03-22 11:44:46 +0530

    Time: 70 ms (7.56%) | Memory: 76.9 MB (5.63%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -4,23 +4,45 @@
  */
 var groupAnagrams = function(strs) {
 
+    // const map = new Map();
+
+    // for(let i=0;i<strs.length;i++) {
+    //     let curr = strs[i].split('').sort().join('');
+
+    //     if(!map.has(curr)) map.set(curr,[]);
+
+    //     map.get(curr).push(strs[i]);
+    // }
+
+    // // console.log(map);
+
+    // // console.log([...map.values()]);
+
+    // return [...map.values()];
+
+    // // or return Array.from(map.values());
+
     const map = new Map();
 
     for(let i=0;i<strs.length;i++) {
-        let curr = strs[i].split('').sort().join('');
 
-        if(!map.has(curr)) map.set(curr,[]);
+        let freqArr = Array(26).fill(0);
+        let currS = strs[i]
+        for(let j=0;j<currS.length;j++) {
 
-        map.get(curr).push(strs[i]);
+            let index = currS[j].charCodeAt() - 'a'.charCodeAt()
+            freqArr[index]++;
+        }
+        let key = "";
+        for(let k=0;k<26;k++) {
+            key+= String.fromCharCode(k)+freqArr[k];
+
+        }
+        if(!map.has(key)) map.set(key,[currS]);
+        else map.get(key).push(currS);
     }
-
-    // console.log(map);
-
-    // console.log([...map.values()]);
-
-    return [...map.values()];
-
-    // or return Array.from(map.values());
+      
+      return [...map.values()];
 
     
 
