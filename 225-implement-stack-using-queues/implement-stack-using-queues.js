29f019d0a9f30b2af8c1a2935750f34289192cd7
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-03-24 19:13:37 +0530

    Time: 0 ms (100.00%) | Memory: 53.1 MB (85.31%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.js
+++ 225-implement-stack-using-queues/implement-stack-using-queues.js
@@ -1,7 +1,6 @@
 
 var MyStack = function() {
     this.q1 = [];
-    this.q2 = [];
     
 };
 
@@ -19,37 +18,25 @@ MyStack.prototype.push = function(x) {
  */
 MyStack.prototype.pop = function() {
     let n = this.q1.length;
-
-    for(i=0;i<n-1;i++) {
-        this.q2.push(this.q1.shift());
-    }
-    let ans = this.q1.shift();
-    let temp =  this.q1 ;
-    this.q1 =  this.q2; 
-    this.q2 = temp;
-
-    return ans;
+    for(let i=0;i<n-1;i++) {
+        this.q1.push(this.q1.shift());
+    } 
+    return this.q1.shift();
+   
 };
 
 /**
  * @return {number}
  */
 MyStack.prototype.top = function() {
-
     let n = this.q1.length;
     for(let i=0;i<n-1;i++) {
-        this.q2.push(this.q1.shift());
+        this.q1.push(this.q1.shift());
     }
-
-    let front = this.q1[0];
-    this.q2.push(this.q1.shift());
-    
-    let temp =  this.q1 ;
-    this.q1 =  this.q2; 
-    this.q2 = temp;
-
-    return front;
-
+    let top = this.q1[0];
+    this.q1.push(top);
+    this.q1.shift();
+    return top;
     
 };
 
@@ -57,8 +44,7 @@ MyStack.prototype.top = function() {
  * @return {boolean}
  */
 MyStack.prototype.empty = function() {
-
-    return this.q1.length===0;
+    return this.q1.length == 0;
     
 };
 
