fd1835dfd42fb832e2b4b4683d3a2fd25314d640
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-03-26 23:37:05 +0530

    Time: 3 ms (70.47%) | Memory: 54.6 MB (91.10%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -3,13 +3,17 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-     let stack = [];
+    let stack = [];
+
     const map = { '(':')','[':']','{':'}' };
 
     for(let i=0;i<s.length;i++){
+
         let currSymbol = s[i];
+
         if(currSymbol =='(' || currSymbol == '[' || currSymbol == '{') {
             stack.push(currSymbol);
+            
         }else {
             if(stack.length == 0 || currSymbol !== map[stack.pop()]) return false;
         }
