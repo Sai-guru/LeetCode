6db5ba3de87768c8a65183c623a67de43e8ae0fb
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-03-26 23:36:35 +0530

    Time: 2 ms (83.80%) | Memory: 54.8 MB (83.14%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    let stack = [];
+     let stack = [];
     const map = { '(':')','[':']','{':'}' };
 
     for(let i=0;i<s.length;i++){
@@ -15,17 +15,5 @@ var isValid = function(s) {
         }
     }
     return stack.length == 0;
-    
-    // let stack = [];
-    // for(let i=0;i<s.length;i++) {
-    //     let currSymbol = s[i];
-    //      if(currSymbol =='(' || currSymbol == '[' || currSymbol == '{') {
-    //         stack.push(currSymbol);
-    //     }else {
-    //         let top = stack.pop();
-    //         if(!top || (top == '(' && currSymbol!== ')'|| top == '[' && currSymbol!== ']' || top == '{' && currSymbol!== '}'))return false;
-    //     }
-
-    // }return stack.length == 0;
     
 };
