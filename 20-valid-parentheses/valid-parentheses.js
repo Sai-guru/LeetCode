d0e5ff437b9348e37f13558b0ba42c5ce31a4021
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-03-29 19:49:30 +0530

    Time: 3 ms (70.44%) | Memory: 54.9 MB (83.07%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -12,11 +12,24 @@ var isValid = function(s) {
         if(curr=='(' || curr == '[' || curr == '{') stack.push(curr);
 
         else{
-            let top = stack.pop();
+        //     let top = stack.pop();
 
-        if(!top || top=='('&& curr!== ")"  ||top=='['&& curr!== "]" || top=='{'&& curr!== "}") return false;
+        // if(!top || top=='('&& curr!== ")"  ||top=='['&& curr!== "]" || top=='{'&& curr!== "}") return false;
+        // }
+        let top = stack.pop();
+        if(!top) return false;
+
+if (top == '(' && curr == ")") {
+    // Match! Do nothing, just let the loop continue
+} else if (top == '[' && curr == "]") {
+    // Match! Do nothing
+} else if (top == '{' && curr == "}") {
+    // Match! Do nothing
+} else {
+    // IF IT REACHES HERE, NOTHING MATCHED
+    return false; 
+}
         }
-        
     }
     return stack.length===0;
     
