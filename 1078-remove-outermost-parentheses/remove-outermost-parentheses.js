f5962b9ddeaddd77d87aa81118fa8ec957e4403a
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-03-30 22:38:43 +0530

    Time: 2 ms (86.95%) | Memory: 57.5 MB (26.78%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.js
@@ -3,20 +3,6 @@
  * @return {string}
  */
 var removeOuterParentheses = function(s) {
-    // let stack = [];
-    // let res = '';
-    //  for(let i=0;i<s.length;i++){
-        
-    //     if(s[i] == '(') {
-    //         if(stack.length>0) res+=s[i];
-    //         stack.push(s[i]);
-            
-    //      }else{
-    //         stack.pop(); 
-    //         if(stack.length>0) res+=s[i];       
-    //     } 
-         
-    //  } return res;
     let lvl = 0;
     let res = '';
     for(let i=0;i<s.length;i++) {
@@ -28,4 +14,5 @@ var removeOuterParentheses = function(s) {
              if(lvl>0) res+=s[i];
         }
     } return res;
-}; 
+    
+};
