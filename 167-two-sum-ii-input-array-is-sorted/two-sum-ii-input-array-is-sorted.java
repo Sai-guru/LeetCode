6771922fcce8609ffe21f3f0aa2b0341f853e73b
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-04-01 21:22:03 +0530

    Time: 409 ms (5.30%) | Memory: 48.3 MB (85.74%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,18 +1,35 @@
 class Solution {
-    public int[] twoSum(int[] arr, int target) {
-        int l = 0;
+    public int[] twoSum(int[] nums, int target) {
+        // int l = 0;
+        // int sum = 0;
+        // int r = arr.length-1;
+
+        // while(l<r) {
+
+        //     sum = arr[l]+arr[r];
+
+        //     if(sum>target) r--;
+        //     else if(sum==target) return new int[] {l+1,r+1};
+        //     else l++;
+        // }
+        // return new int[] {-1,-1};
+
         int sum = 0;
-        int r = arr.length-1;
+       
 
-        while(l<r) {
+        for(int i=0;i<nums.length;i++) {
 
-            sum = arr[l]+arr[r];
+            for(int j=i+1;j<nums.length;j++) {
+                 
+                    sum = nums[i]+nums[j];
 
-            if(sum>target) r--;
-            else if(sum==target) return new int[] {l+1,r+1};
-            else l++;
+                    if(sum==target) {
+                       return new int[] {i+1,j+1};
+                    }
+            }
+
         }
-        return new int[] {-1,-1};
+        return new int[]{-1, -1};
         
     }
 }
