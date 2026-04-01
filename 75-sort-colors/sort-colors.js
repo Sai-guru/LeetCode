0bfdd8525b4f04789cca6a6fddd597e6fcd5b439
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-04-01 23:13:42 +0530

    Time: 1 ms (27.53%) | Memory: 54.1 MB (42.00%) - LeetSync

--- 75-sort-colors/sort-colors.js
+++ 75-sort-colors/sort-colors.js
@@ -2,8 +2,27 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var sortColors = function(nums) {
+var sortColors = function(arr) {
 
-    return nums.sort();
-    
-};
+    // return nums.sort();
+    let low = curr = 0;
+    let high = arr.length-1;
+
+    while(curr<=high) {
+
+        if(arr[curr]==0) {
+
+            // let temp = arr[low];
+            // arr[low] = arr[curr];
+            // arr[curr] = temp;
+            [arr[low],arr[curr]] = [arr[curr],arr[low]];
+            low++;curr++
+
+        }else if(arr[curr]==2) {
+            
+            [arr[high],arr[curr]] = [arr[curr],arr[high]];
+            high--;
+
+        }else curr++;
+    } return arr;
+}
