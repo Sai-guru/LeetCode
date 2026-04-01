2adfa8383d14525b1c4cbb6f15bfb955c2fb600e
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-04-01 21:28:10 +0530

    Time: 2 ms (96.36%) | Memory: 48.5 MB (33.29%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -1,35 +1,35 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        // int l = 0;
-        // int sum = 0;
-        // int r = arr.length-1;
-
-        // while(l<r) {
-
-        //     sum = arr[l]+arr[r];
-
-        //     if(sum>target) r--;
-        //     else if(sum==target) return new int[] {l+1,r+1};
-        //     else l++;
-        // }
-        // return new int[] {-1,-1};
-
+    public int[] twoSum(int[] arr, int target) {
+        int l = 0;
         int sum = 0;
+        int r = arr.length-1;
+
+        while(l<r) {
+
+            sum = arr[l]+arr[r];
+
+            if(sum>target) r--;
+            else if(sum==target) return new int[] {l+1,r+1};
+            else l++;
+        }
+        return new int[] {-1,-1};
+
+        // int sum = 0;
        
 
-        for(int i=0;i<nums.length;i++) {
+        // for(int i=0;i<nums.length;i++) {
 
-            for(int j=i+1;j<nums.length;j++) {
+        //     for(int j=i+1;j<nums.length;j++) {
                  
-                    sum = nums[i]+nums[j];
+        //             sum = nums[i]+nums[j];
 
-                    if(sum==target) {
-                       return new int[] {i+1,j+1};
-                    }
-            }
+        //             if(sum==target) {
+        //                return new int[] {i+1,j+1};
+        //             }
+        //     }
 
-        }
-        return new int[]{-1, -1};
+        // }
+        // return new int[]{-1, -1};
         
     }
 }
