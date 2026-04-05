96cd84bd6c13ff5bd07a15510864e7004c29f377
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-04-05 10:23:06 +0530

    Time: 2 ms (86.74%) | Memory: 62 MB (74.98%) - LeetSync

---
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function(nums, k) {
+
+    let l = maxC = 0;
+    let zCount = 0;
+
+    for(let i=0;i<nums.length;i++){
+
+        if(nums[i]==0) zCount++;
+
+        while(zCount>k){
+            if(nums[l]==0) zCount--;
+            l++
+        }
+
+        maxC = Math.max(maxC,i-l+1);
+    }
+    return maxC;
+    
+};
