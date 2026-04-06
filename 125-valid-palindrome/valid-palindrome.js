999e57fbce1ca581e4765985c2b4fd9b74461c4a
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-04-06 20:47:06 +0530

    Time: 10 ms (17.68%) | Memory: 64.4 MB (6.44%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -4,69 +4,19 @@
  */
 var isPalindrome = function(s) {
 
-    //Approach 2 - better SC optimal...
-    //2 pts approach
+    s=s.toLowerCase();
+    let filteredStr = revStr = '';
 
-    s = s.toLowerCase();
+    for(let i=0;i<s.length;i++) {
 
-    let i = 0;
-    let j = s.length-1;
+       if(s[i]>='a' && s[i]<='z' || s[i]>='0' && s[i]<='9') {
+        filteredStr +=s[i];
+        revStr = s[i]+revStr;
 
-    while(i<j) {
+       }
 
-        if(!s[i].match(/[a-z0-9]/)) i++;
-        else if (!s[j].match(/[a-z0-9]/)) j--;
-
-        else if(s[i]==s[j]) {
-            i++;j--;
-        }
-        else return false
-
-       
     }
-    return true;
+    return filteredStr === revStr;
 
-
-
-
-
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Approach 1 
-
-    //  s = s.toLowerCase();
-
-    //filtered value 
-    // let filteredStr = "";
-    // let revStr = "";
-
-    // for(let i=0;i<s.length;i++) {
-        // if(s[i].match(/[a-z0-9]/i)) filteredStr += s[i];
-        // if((s[i]>='a' && s[i]<='z') || (s[i]>='0' && s[i]<='9'))  {
-        //     filteredStr += s[i];
-        //     revStr = s[i] + revStr
-        // }
-
-
-    // }
-    // console.log(filteredStr)
-
-    // return filteredStr===filteredStr.split('').reverse().join('');
-
-    // return filteredStr===revStr;
-
-}; 
+    
+};
