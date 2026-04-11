8c4202160fa4c2c4598aa20e424093319719e4b7
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-04-11 15:57:09 +0530

    Time: 3 ms (20.20%) | Memory: 42.9 MB (80.73%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -5,7 +5,7 @@ class Solution {
 
     private String readyStr(String str) {
         // approach 1 - using stack
-        /*
+        
         Stack<Character> stack = new Stack<>();
 
         for (int i=0; i<str.length();i++) {
@@ -16,25 +16,25 @@ class Solution {
         }
         // return stack.join('');
         return stack.toString();
-        */
+       
 
         //approach 2 - using Map (cool and easy)...
-        Map<Integer, Character> map = new HashMap<>();
-        int orderKey = 0;
+        // Map<Integer, Character> map = new HashMap<>();
+        // int orderKey = 0;
 
-        for (int i=0; i<str.length();i++) {
-            char curr = str.charAt(i);
+        // for (int i=0; i<str.length();i++) {
+        //     char curr = str.charAt(i);
 
-            if (curr!='#') {
-                map.put(orderKey,curr);
-                orderKey++;
-            } else if (orderKey > 0) {
-                orderKey--;
-                map.remove(orderKey);
-            }
-        }
+        //     if (curr!='#') {
+        //         map.put(orderKey,curr);
+        //         orderKey++;
+        //     } else if (orderKey > 0) {
+        //         orderKey--;
+        //         map.remove(orderKey);
+        //     }
+        // }
 
         // return [...map.values()].join('');
-        return new ArrayList<>(map.values()).toString();
+    //     return new ArrayList<>(map.values()).toString();
     }
 }
