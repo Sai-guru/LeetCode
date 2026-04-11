16b8f768c4ed9c01806890c158affcf5b9eb7f54
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-04-11 15:53:52 +0530

    Time: 3 ms (20.20%) | Memory: 43.6 MB (8.70%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        return readyStr(s).equals(readyStr(t));
+    }
+
+    private String readyStr(String str) {
+        // approach 1 - using stack
+        /*
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+
+            if (curr != '#') stack.push(curr);
+            else if (!stack.isEmpty()) stack.pop();
+        }
+        // return stack.join('');
+        return stack.toString();
+        */
+
+        //approach 2 - using Map (cool and easy)...
+        Map<Integer, Character> map = new HashMap<>();
+        int orderKey = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+
+            if (curr != '#') {
+                map.put(orderKey, curr);
+                orderKey++;
+            } else if (orderKey > 0) {
+                orderKey--;
+                map.remove(orderKey);
+            }
+        }
+
+        // return [...map.values()].join('');
+        return new ArrayList<>(map.values()).toString();
+    }
+}
