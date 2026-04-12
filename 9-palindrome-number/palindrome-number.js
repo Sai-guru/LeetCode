90d2392a3a20c290459386b8e2b9168a12773228
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-04-12 21:21:07 +0530

    Time: 8 ms (46.04%) | Memory: 64.2 MB (52.67%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,16 +3,16 @@
  * @return {boolean}
  */
 var isPalindrome = function(n) {
+
     let rev = 0;
-    let copyOfn = n
+    let revCopy = n;
 
-    if(n<0) return false;
+    while(n>0){
 
-    while(n>0) {
         let rem = n%10;
-        rev = (10*rev)+rem;
-        n =Math.floor(n/10);
+        rev = 10*rev + rem;
+        n=Math.floor(n/10);
     }
-    return rev===copyOfn;
+    return rev===revCopy;
     
 };
