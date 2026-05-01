3a0cc61fa79b118f4e99d426d72cc1fb05131592
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-05-01 15:51:50 +0530

    Time: 2 ms (74.27%) | Memory: 56.9 MB (59.94%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -5,13 +5,20 @@
 var majorityElement = function(nums) {
 
     const map = new Map();
+    
+    // for(let num of nums) map.set(num,(map.get(num) || 0)+1);
 
-    for(let num of nums) map.set(num,(map.get(num) || 0)+1);
+    // for(let [key,count] of map){
 
-    for(let [key,count] of map){
+    //     if(count > nums.length/2) return key;
+    // }
 
-        if(count > nums.length/2) return key;
-    }
+for (let num of nums) {
+    
+    map.set(num, (map.get(num) || 0) + 1);
+    if (map.get(num) > nums.length / 2) return num;
+   
+}
 
 
 
