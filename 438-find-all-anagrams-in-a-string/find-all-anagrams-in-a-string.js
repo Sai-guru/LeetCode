75fd0861ab63399f457db996880305bc6553b95f
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-05-01 15:06:39 +0530

    Time: 18 ms (65.05%) | Memory: 60 MB (69.80%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.js
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.js
@@ -5,24 +5,47 @@
  */
 var findAnagrams = function(s, p) {
 
-    let res = [] ,map = {} ,c = p.length;
+    let res = [];
+    const map = new Map();
     let l = r = 0;
-    
+    let winS = p.length;
 
-    for (let i=0;i<p.length;i++){
-        let curr = p[i];
-        map[curr] = (map[curr] || 0)+1;
-    }
+    for(let x of p) map.set(x,(map.get(x) || 0)+1);
 
     while(r<s.length){
+        let rCurr = s[r];
 
-        if(map[s[r++]]-- >0) c--;
+        if(map.has(rCurr) && map.get(rCurr)>0) winS--;
 
-        if(c==0) res.push(l);
+        map.set(rCurr,(map.get(rCurr)||0)-1);
+        r++;
 
-        if(r-l==p.length) {
-             if(map[s[l++]]++ >=0) c++;
+
+        //success checkk
+        if(winS==0) res.push(l);
+
+        // after pushing , relieve the left most element/..
+
+        if(r-l == p.length){
+            leftCurr = s[l];
+
+            if(map.has(leftCurr) && map.get(leftCurr)>=0) winS++;
+
+            map.set(leftCurr,(map.get(leftCurr)||0)+1);
+            l++;
         }
-    } return res;
-
+    }
+    return res;
 };
+
+
+
+
+
+
+
+
+
+
+
+
