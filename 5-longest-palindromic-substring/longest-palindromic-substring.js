fae01e8ed9ebd8c1943a21113062e8efccd6c07f
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-05-05 22:51:43 +0530

    Time: 21 ms (54.05%) | Memory: 56.9 MB (44.15%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -7,11 +7,10 @@ var longestPalindrome = function(s) {
     let res = '';
     let resLen = 0;
 
-    for(let i=0;i<s.length;i++) {
-
+    
+         function expand(l, r) {
         //odd length...
-
-        let l = r = i;
+        // let l = r = i;
         while(l>=0 && r<s.length && s[l]==s[r]){
 
             if(r-l+1>=resLen){
@@ -21,22 +20,24 @@ var longestPalindrome = function(s) {
             }
         l--;r++;  //note when l=0 , doing l-- is still 0 only...
         }
+}
         
-        //even length...
-        l = i;r=i+1;
-        while(l>=0 && r<s.length && s[l]==s[r]){
+        // //even length...
+        // l = i;r=i+1;
+        // while(l>=0 && r<s.length && s[l]==s[r]){
 
-            if(r-l+1>=resLen){
+        //     if(r-l+1>=resLen){
 
-            res = s.substring(l,r+1);
-            resLen = r-l+1;
-            }
-        l--;r++;
-        }
+        //     res = s.substring(l,r+1);
+        //     resLen = r-l+1;
+        //     }
+        // l--;r++;
+        // }
 
-
-
+        for (let i = 0; i < s.length; i++) {
+            expand(i, i);       // Odd length
+        expand(i, i + 1);   // Even length
     }
+
     return res;
-    
 };
