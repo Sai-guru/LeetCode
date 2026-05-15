c79c1d356c1b82254383c86a00288615ee5adc27
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-05-15 21:43:05 +0530

    Time: 18 ms (80.78%) | Memory: 56.9 MB (44.14%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -8,23 +8,20 @@ var longestPalindrome = function(s) {
 
     // The helper function using your exact expansion logic
     function expand(l, r) {
-        while (l >= 0 && r < s.length && s[l] == s[r]) {
-            if (r - l + 1 > resLen) {
+        while (l >= 0 && r <s.length && s[l]==s[r]) {
+            if (r-l+1>resLen) {
                 res = s.substring(l, r + 1);
-                resLen = r - l + 1;
+                resLen = r-l+1;
             }
-            l--; 
-            r++;
+            l--;  r++;
         }
     }
 
-    for (let i = 0; i < s.length; i++) {
+    for (let i=0;i< s.length;i++) {
         // odd length
-        expand(i, i);
-        
+        expand(i,i);
         // even length
-        expand(i, i + 1);
+        expand(i,i+1);
     }
-
     return res;
 };
