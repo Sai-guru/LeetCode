05ad23076ce7c402658befa9e4b7f6e7f5eba5ef
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-07-17 18:22:44 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (79.86%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,21 +1,19 @@
-
-import java.math.BigInteger;
 class Solution {
     public int[] plusOne(int[] digits) {
 
-        String x = "";
+        int[] res = new int[digits.length+1];
 
-        for(int i=0;i<digits.length;i++) x+=digits[i];
+        for(int i=digits.length-1;i>=0;i--){
+
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+            
+            res[0]=1;
         
-
-        BigInteger res = new BigInteger(x);
-        res = res.add(BigInteger.ONE);
-
-        // System.out.println(res);
-
-        return res.toString().chars().map(Character :: getNumericValue).toArray();
-
-
-        
+        return res;
     }
 }
