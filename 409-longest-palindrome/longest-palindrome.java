8697b337c1590589944c7f9c11200c0019133a6e
prigeesh <165147568+Sai-guru@users.noreply.github.com>
2026-07-22 21:55:28 +0530

    Time: 8 ms (28.62%) | Memory: 43.2 MB (57.46%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int longestPalindrome(String s) {
+
+        Map<Character,Integer> map = new LinkedHashMap<>();
+        int res = 0;
+
+        for(int i=0;i<s.length();i++) {
+            char curr = s.charAt(i);
+            map.put(curr,map.getOrDefault(curr,0)+1);
+        }
+        
+        int oddC = 0;
+        for(int curr : map.values()){
+            if(curr%2==0) res+=curr;
+            else {
+                res = res+curr-1;
+                oddC++;
+            }
+        }
+
+        if(oddC>0) res++;
+
+
+        return res;
+    }
+}
